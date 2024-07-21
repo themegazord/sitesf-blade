cd608e871700ecb0de6fc0e638a74a8031bf79b8
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2024-07-21 14:05:26 -0400

    chore: inserido titulo das paginas de barrestaurante e parceiros

--- app/Livewire/Views/Solucoes/BarRestaurante.php
+++ app/Livewire/Views/Solucoes/BarRestaurante.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire\Views\Solucoes;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('SF Sistemas - Solucao para bares e restaurantes')]
 class BarRestaurante extends Component
 {
     public function render()

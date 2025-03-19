1ff7e9647b4bd68dd0a81a0578f11b7078835992
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-03-19 10:29:48 -0400

    adicionado rota para consultar os dados do json

--- app/Http/Controllers/ControleObito.php
+++ app/Http/Controllers/ControleObito.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ControleObito extends Controller
 {
@@ -22,8 +23,27 @@ class ControleObito extends Controller
     return response()->json(['message' => 'JSON salvo com sucesso', 'file' => $nomeArquivo]);
   }
 
-  // public function show(): JsonResponse
-  // {
+  public function show(string $nome_arquivo): StreamedResponse|JsonResponse
+  {
+    $caminho = "pharma/{$nome_arquivo}.json";
 
-  // }
+    // Verifica se o arquivo existe
+    if (!Storage::exists($caminho)) {
+      return response()->json(['error' => 'Arquivo não encontrado'], 404);
+    }
+
+    // Retorna um Stream de resposta para evitar carregamento em memória
+    return response()->stream(function () use ($caminho) {
+      $handle = Storage::readStream($caminho);
+      while (!feof($handle)) {
+        echo fread($handle, 1024 * 8); // Lê e envia em blocos de 8KB
+        flush(); // Envia os dados imediatamente para o cliente
+      }
+      fclose($handle);
+    }, 200, [
+      'Content-Type' => 'application/json',
+      'Cache-Control' => 'no-cache',
+      'Content-Disposition' => 'inline; filename="' . basename($caminho) . '"',
+    ]);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
   Route::prefix('ControleObito')->group(function () {
-    Route::post('envia', [ControleObito::class, 'store'])->name('api.v1.controle_obito.envia');
+    Route::post('envia', [ControleObito::class, 'store'])->name('api.v1.controle_obito.store');
+    Route::get('/{nome_arquivo}', [ControleObito::class, 'show'])->name('api.v1.controle_obito.show');
   });
 });

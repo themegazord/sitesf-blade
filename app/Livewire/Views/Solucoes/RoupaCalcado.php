ebf61e52fae1fff5ba83ac3f7a011414ad9634cb
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-26 11:26:24 -0400

    refact: roupas e calcados

--- app/Livewire/Views/Solucoes/RoupaCalcado.php
+++ app/Livewire/Views/Solucoes/RoupaCalcado.php
@@ -8,6 +8,12 @@ use Livewire\Component;
 #[Title('SF Sistemas - Solucoes - Roupas e Calcados')]
 class RoupaCalcado extends Component
 {
+  public $tags = [
+    'Confecções',
+    'Lingeries',
+    'Roupas Infantis'
+  ];
+
   public function render()
   {
     return view('livewire.views.solucoes.roupa-calcado');

4934f9e8ff414f5796b34159bfea1944888ad6fb
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-26 11:37:50 -0400

    refact: distribuidora

--- app/Livewire/Views/Solucoes/DistribuidoraConveniencia.php
+++ app/Livewire/Views/Solucoes/DistribuidoraConveniencia.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 
 class DistribuidoraConveniencia extends Component
 {
+  public $tags = [
+    'Conveniências',
+    'Mini-Mercado',
+    'Armazém',
+    'Distribuidora de Gás e Água'
+  ];
   #[Title("SF Sistemas - Soluções - Distribuidora e Conveniencias")]
   public function render()
   {

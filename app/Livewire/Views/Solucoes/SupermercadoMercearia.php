2628a68825295b9503c91d97cc7ca62b5fb28250
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-26 11:22:44 -0400

    refact: supermercado

--- app/Livewire/Views/Solucoes/SupermercadoMercearia.php
+++ app/Livewire/Views/Solucoes/SupermercadoMercearia.php
@@ -8,6 +8,13 @@ use Livewire\Component;
 #[Title('SF Sistemas - Solucoes - Supermercado e Mercearias')]
 class SupermercadoMercearia extends Component
 {
+  public $tags = [
+    'Mercearias',
+    'Supermercados',
+    'Bolichós',
+    'Sacolão de Frutas e Verduras',
+    'Mercadinhos'
+  ];
   public function render(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
   {
     return view('livewire.views.solucoes.supermercado-mercearia');

5d55a01fed3d080a809ee4714cb09403f20aaa37
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-26 10:59:01 -0400

    refact: bar e solucoes

--- app/Livewire/Views/Solucoes/BarRestaurante.php
+++ app/Livewire/Views/Solucoes/BarRestaurante.php
@@ -8,6 +8,15 @@ use Livewire\Component;
 #[Title('SF Sistemas - Solucao para bares e restaurantes')]
 class BarRestaurante extends Component
 {
+  public $tags = [
+    'Bares',
+    'Lanchonetes',
+    'Pizzarias',
+    'Marmitarias',
+    'Soberias',
+    'Restaurantes em Geral'
+  ];
+
   public function render(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
   {
     return view('livewire.views.solucoes.bar-restaurante');

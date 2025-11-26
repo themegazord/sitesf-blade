d36d9c642a8613e5247b482d3a3b6e4766078414
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2025-11-26 12:01:52 -0400

    refact: parceiros

--- app/Livewire/Views/Parceiros.php
+++ app/Livewire/Views/Parceiros.php
@@ -8,6 +8,15 @@ use Livewire\Component;
 #[Title('SF Sistemas - Parceiros')]
 class Parceiros extends Component
 {
+  public $parceiros = [
+    ['nome' => 'Deck Café Bar', 'logo' => 'imagens/Parceiros/DeckCafeBar.png'],
+    ['nome' => 'HeT', 'logo' => 'imagens/Parceiros/HeT.png'],
+    ['nome' => 'Lagoa', 'logo' => 'imagens/Parceiros/Lagoa.png'],
+    ['nome' => 'Mafisa', 'logo' => 'imagens/Parceiros/Mafisa.png'],
+    ['nome' => 'Obrativa', 'logo' => 'imagens/Parceiros/Obrativa.png'],
+    ['nome' => 'Pax Nacional', 'logo' => 'imagens/Parceiros/PaxNacional.png'],
+    ['nome' => 'Santo Antonio', 'logo' => 'imagens/Parceiros/StAntonio.png'],
+  ];
   public function render(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
   {
     return view('livewire.views.parceiros');
